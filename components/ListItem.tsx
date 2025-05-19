440d113bc58ac6cbdc568cd55c8545b6f5fc988e
Sujal-Khera <sujalkhera.c@gmail.com>
2025-05-20 00:49:04 +0530

    Adding ListItem component

---
+++ components/ListItem.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Image from "next/image"
+import { FaPlay } from "react-icons/fa";
+
+interface ListenItemProps {
+    image : string;
+    name : string;
+    href : string;
+}
+
+const ListItem : React.FC<ListenItemProps> = ({image , name , href}) => {
+    const Router = useRouter();
+    const onClick = () =>{
+        // Add authentication before push
+        Router.push(href);
+    }
+
+    return(
+        <button 
+            className="
+            relative
+            group
+            flex
+            items-center
+            rounded-md
+            overflow-hidden
+            gap-x-4
+            bg-neutral-100/10
+            hover:bg-neutral-100/20
+            transition
+            pr-4"
+        >
+            <div
+                className="
+                relative
+                min-h-[64px]
+                min-w-[64px]
+                "
+            >
+                <Image 
+                    className="object-cover"
+                    fill
+                    src = {image}
+                    alt = "Liked Songs"
+                />
+            </div>
+            <p className="font-medium truncate py-5">
+                {name}
+            </p>
+            <div
+                className="
+                absolute
+                transition
+                opacity-0
+                rounded-full
+                flex
+                items-center
+                justify-content
+                bg-green-500
+                p-4
+                drop-shadow-md
+                right-5
+                group-hover:opacity-100
+                hover:scale-110"
+            >
+                <FaPlay className="text-black" />
+            </div>
+        </button>
+    );
+}
+
+export default ListItem;
